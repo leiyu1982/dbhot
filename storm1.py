75e95f6780e6e9325c975705017e4b0d45fbe697
leiyu <leiyu530@163.com>
2020-07-20 13:03:10 +0800

    add storm1.py file

---
+++ storm1.py
@@ -0,0 +1,12 @@
+
+Disk /dev/sda: 53.7 GB, 53687091200 bytes, 104857600 sectors
+Units = sectors of 1 * 512 = 512 bytes
+Sector size (logical/physical): 512 bytes / 512 bytes
+I/O size (minimum/optimal): 512 bytes / 512 bytes
+Disk label type: dos
+Disk identifier: 0x000a16ea
+
+   Device Boot      Start         End      Blocks   Id  System
+/dev/sda1   *        2048      616447      307200   83  Linux
+/dev/sda2          616448     8744959     4064256   82  Linux swap / Solaris
+/dev/sda3         8744960   104857599    48056320   83  Linux
